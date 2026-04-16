3fbbb8fbed1d06875d5710fc70bbcd736d61ca3b
brunorojo15 <brunorojo15@gmail.com>
2026-04-16 16:41:25 -0300

    Tp
    Subo practico de unidad 3.

---
+++ tp.estructuras.repetitivas.1.py
@@ -0,0 +1,4 @@
+#Trabajo Practico N°1 - Estructuras Repetitivas
+#Ejercicio N°1-"Caja del Kiosco"
+nombre_cliente=input("Ingrese el nombre del cliente: ").alpha()
+print("Bienvenido a la caja del kiosco",nombre_cliente)
